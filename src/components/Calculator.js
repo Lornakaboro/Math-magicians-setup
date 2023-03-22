3f7b4be413f140a4a1157aa37baf20574efd69c3
Lornakaboro <lornakaboro@gmail.com>
2023-03-22 23:53:43 +0300

    Change class components to functional components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,50 +1,53 @@
 import React from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="calculator">
-        <div>
-          <input placeholder="0" className="result" />
-        </div>
-        <div className="digits">
-          <button type="button" className="digit-btn">AC</button>
-          <button type="button" className="digit-btn">+/-</button>
-          <button type="button" className="digit-btn">%</button>
-          <button type="button" className="press-btn amber">÷</button>
-        </div>
-        <div className="digits">
-          <button type="button" className="digit-btn">7</button>
-          <button type="button" className="digit-btn">8</button>
-          <button type="button" className="digit-btn">9</button>
-          <button type="button" className="press-btn amber">X</button>
-        </div>
-        <div className="digits">
-          <button type="button" className="digit-btn">6</button>
-          <button type="button" className="digit-btn">5</button>
-          <button type="button" className="digit-btn">4</button>
-          <button type="button" className="press-btn amber">-</button>
-        </div>
-        <div className="digits">
-          <button type="button" className="digit-btn">3</button>
-          <button type="button" className="digit-btn">2</button>
-          <button type="button" className="digit-btn">1</button>
-          <button type="button" className="press-btn amber">+</button>
-        </div>
-        <div className="operators">
-          <button type="button" className="btn-o">0</button>
-          <button type="button" className="press-btn">.</button>
-          <button type="button" className="press-btn amber">=</button>
-        </div>
-
-      </div>
-    );
-  }
+function Calculator() {
+  return (
+    <CalculatorUi />
+  );
 }
+
+const CalculatorUi = () => {
+  const digits = ['AC', '+/-', '%', 7, 8, 9, 4, 5, 6, 3, 2, 1, 0, '.'];
+
+  const operators = ['÷', 'X', '-', '+', '='];
+
+  const digitButtons = digits.map((digit) => (
+    <button className="digit-btn" key={digit} type="button">{digit}</button>
+  ));
+
+  const operatorButtons = operators.map((operator) => (
+    <button className="press-btn amber" key={operator} type="button">{operator}</button>
+  ));
+
+  return (
+    <div className="calculator">
+      <div>
+        <input placeholder="0" className="result" />
+      </div>
+      <div className="digits">
+        {digitButtons.slice(0, 3)}
+        {operatorButtons.slice(0, 1)}
+      </div>
+      <div className="digits">
+        {digitButtons.slice(3, 6)}
+        {operatorButtons.slice(1, 2)}
+      </div>
+      <div className="digits">
+        {digitButtons.slice(6, 9)}
+        {operatorButtons.slice(2, 3)}
+      </div>
+      <div className="digits">
+        {digitButtons.slice(9, 10)}
+        {digitButtons.slice(10, 12)}
+        {operatorButtons.slice(3, 4)}
+      </div>
+      <div className="operators">
+        <button type="button" className="btn-o">{digits[12]}</button>
+        <button type="button" className="digit-btn">{digits[13]}</button>
+        {operatorButtons.slice(4, 5)}
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
