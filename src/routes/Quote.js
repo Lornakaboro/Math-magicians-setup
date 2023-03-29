6812863f4080af86603e7e5d79391fb7415ebea8
Lornakaboro <lornakaboro@gmail.com>
2023-03-29 18:24:17 +0300

    Add quote route

---
+++ src/routes/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import QuoteGenerator from '../components/Quote';
+
+export default function Quote() {
+  return (
+    <>
+      <p className="Quote-description">
+        Mathematics is not about numbers, equations, computations, or algorithms:
+        it is about understanding. -William Paul Thurston
+      </p>
+    </>
+  );
+}
